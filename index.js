ef99723c080054171e2192be19f81026f4ae05a9
Abubakar Zorrain <abubakarzorrain71@gmail.com>
2021-09-15 12:25:20 +0500

    Its is toDoApp....

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const app = express()
 const fs = require('fs')
 const dataPath ='./file.json'  // path to our JSON file
 const bodyParser = require("body-parser")
+const ejs = require('ejs')
+const flash = require('express-flash')
+const session = require('express-session')
+const hbs				= require('express-handlebars');
+
+app.engine('hbs', hbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+
+app.use(session({
+	secret: "verygoodsecret",
+	resave: false,
+	saveUninitialized: true
+}));
 // app.use(express.json()
 // middleware
 app.use(bodyParser.json())
@@ -20,13 +37,22 @@ const getAccountData = () => {
     const jsonData = fs.readFileSync(dataPath)
     return JSON.parse(jsonData)   
 }
-
+app.get('/login', (req, res) => {
+  res.render('index')
+})
 app.get('/', (req, res) => {
-
+  // const accounts = getAccountData();
+  // const response = {
+  //   title: "ToDoApp",
+  //   error: req.query.error,
+  //   message : "Added Sucessfully",
+  //   accounts
+  // };console.log(accounts)
+  // res.render('index',response)
     res.send('hello world!')
 })
-app.post('/account/addaccount', (req, res) => {
- 
+app.post('/addtodo', (req, res) => {
+  try{
     var existAccounts = getAccountData()
     const newAccountId = Math.floor(100000 + Math.random() * 900000)
  
@@ -34,25 +60,49 @@ app.post('/account/addaccount', (req, res) => {
    
     console.log(existAccounts);
     saveAccountData(existAccounts);
+    // const accounts = getAccountData()
+    // console.log(accounts)
+    
+    // const response = {
+    //   title: "ToDoApp",
+    //   error: req.query.error,
+    //   message : "Added Sucessfully",
+    //   accounts
+    // }
+    
+    //res.render('index', response);
     res.send({success: true, msg: 'account added successfully'})
+  }catch(error){
+    res.send(error)
+  }
 })
 // Read - get all accounts from the json file
-app.get('/account/list', (req, res) => {
+app.get('/alltodo', (req, res) => {
     const accounts = getAccountData()
     res.send(accounts)
   })
-app.put('/account/:id', (req, res) => {
+app.put('/updatetodo/:id', (req, res) => {
     var existAccounts = getAccountData()
     fs.readFile('file.json', 'utf8', (err, data) => {
-      const accountId = req.params['id'];
+      const accountId = req.params.id;
+      console.log(req.body.username);
+      console.log(accountId);
+     console.log( parseInt(accountId));
+
+
+      //existAccounts[accountId].username='mununununu'
+      //console.log(existAccounts[accountId].username);
       existAccounts[accountId] = req.body;
-      saveAccountData(existAccounts);
+      // existAccounts[accountId].username = req.body.username;
+      // console.log(existAccounts[accountId].username)
+      saveAccountData(existAccounts); 
+      // delete existAccounts[accountId];
       res.send(`accounts with id ${accountId} has been updated`)
     }, true);
   });
 
   // delete - using delete method
-app.delete('/account/delete/:id', (req, res) => {
+app.delete('/todo/delete/:id', (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
       var existAccounts = getAccountData()
       const userId = req.params['id'];
